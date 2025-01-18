9378eb8fb060d76ab0397b666fb5b8bb090b17a4
Guilherme Emetério <guilhermeemeterioif@gmail.com>
2025-01-18 14:11:29 -0300

    Adicionado configuração do wokwi e junção da lógica para selecionar o comando do usuário, assim como a função para dar reboot na placa via USB.

--- tarefa-2.c
+++ tarefa-2.c
@@ -1,14 +1,43 @@
 #include <stdio.h>
+#include <string.h>  // Incluído para usar a função strcmp
 #include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "pico/bootrom.h" 
 
+void reiniciar_boot() {
+    reset_usb_boot(0,0);
+}
 
-
-int main()
-{
+int main() {
+    char comando[10];
     stdio_init_all();
 
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+    /*
+        ToDo
+        Recerber o comando do usuário
+    */
+    
+    while (1) {
+        
+        // Interpretando os comandos
+        if (strcmp(comando, "verde") == 0) {
+            ligar_led_verde();
+        } else if (strcmp(comando, "azul") == 0) {
+            ligar_led_azul();
+        } else if (strcmp(comando, "vermelho") == 0) {
+            ligar_led_vermelho();
+        } else if (strcmp(comando, "branco") == 0) {
+            ligar_leds_branco();
+        } else if (strcmp(comando, "desligar") == 0) {
+            desligar_leds();
+        } else if (strcmp(comando, "buzzer") == 0) {
+            acionar_buzzer();
+        } else if (strcmp(comando, "reboot") == 0) {
+            reiniciar_boot();
+        } else {
+            printf("Comando não reconhecido!\n");
+        }
     }
+
+    return 0;
 }
